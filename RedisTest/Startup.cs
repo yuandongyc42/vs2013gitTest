60a6fcdf26b4aac70232010bf6aa6ba277cfa4fc
yuandongyc42 <yuandongyc@126.com>
2016-10-26 14:10:01 +0800

    第一次创建的提交

---
+++ RedisTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RedisTest.Startup))]
+namespace RedisTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ RedisTest/redisM/CacheManager.cs
@@ -0,0 +1,34 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using Castle.MicroKernel.Registration;
+
+namespace RedisTest.redisM
+{
+    public class CacheManager
+    {
+
+        public static void Init()
+        {
+            var container = IocManager.IocContainer;
+            var si = IocManager.Resolve<SiteInfo>();
+            //注册redis
+            if (!container.Kernel.HasComponent(typeof(ConnectionMultiplexer)))
+                container.Register(Component.For<ConnectionMultiplexer>().UsingFactoryMethod((ioc) =>
+                {
+                    return ConnectionMultiplexer.Connect(si.RedisHost);
+                }).LifestyleSingleton());
+
+
+            if (!container.Kernel.HasComponent(typeof(IDatabase)))
+                container.Register(Component.For<IDatabase>().UsingFactoryMethod((ioc) =>
+                    ioc.Resolve<ConnectionMultiplexer>().GetDatabase())
+                    .LifestylePerWebRequest());
+            ConnectionMultiplexer.Connect(si.RedisHost + ",AllowAdmin=true").GetServer(si.RedisHost).FlushDatabase();
+        }
+
+    }
+}
